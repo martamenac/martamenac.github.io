78504276e2679d31ba7d061924620013489a28f9
FlyingShieldStudio <marta@flyingshieldstudio.com>
2021-04-12 23:47:57 +0200

    Castellnins

---
+++ castellnins/data.js
@@ -0,0 +1,743 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -2.6240520456430456,
+        "pitch": 0.12027699697880401,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.635793400657482,
+          "pitch": 0.1528134503858123,
+          "rotation": 0,
+          "target": "8-passads"
+        },
+        {
+          "yaw": -2.230611616437029,
+          "pitch": 0.1732267521926456,
+          "rotation": 0,
+          "target": "2-estana-dels-tabalers"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-canviador",
+      "name": "Canviador",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2576320037042485,
+          "pitch": 0.20577973684032713,
+          "rotation": 0,
+          "target": "2-estana-dels-tabalers"
+        },
+        {
+          "yaw": -1.1014859702235924,
+          "pitch": 0.09649020639010786,
+          "rotation": 0,
+          "target": "3-estana-dels-nans"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-estana-dels-tabalers",
+      "name": "Estança dels Tabalers",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -1.8461956827914037,
+        "pitch": 0.2824651256615809,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.962822183072788,
+          "pitch": 0.24281782742955293,
+          "rotation": 0,
+          "target": "1-canviador"
+        },
+        {
+          "yaw": 1.7010471263162659,
+          "pitch": 0.21559779584365124,
+          "rotation": 0,
+          "target": "8-passads"
+        },
+        {
+          "yaw": -1.6400109771714249,
+          "pitch": 0.038128230938179186,
+          "rotation": 0,
+          "target": "9-pati-gran"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-estana-dels-nans",
+      "name": "Estança dels Nans",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 0.6880481353910461,
+        "pitch": 0.29380694741509217,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5249799074024306,
+          "pitch": 0.20832748486713015,
+          "rotation": 0,
+          "target": "8-passads"
+        },
+        {
+          "yaw": 0.4358976434091595,
+          "pitch": 0.09338709793034283,
+          "rotation": 0,
+          "target": "1-canviador"
+        },
+        {
+          "yaw": -0.29825364988015934,
+          "pitch": 0.01440900986234439,
+          "rotation": 0,
+          "target": "10-pati-gran"
+        },
+        {
+          "yaw": -2.4169442982472553,
+          "pitch": 0.2387810102604604,
+          "rotation": 0,
+          "target": "7-dormitori"
+        },
+        {
+          "yaw": -1.1999532083336835,
+          "pitch": 0.13570436075835168,
+          "rotation": 0,
+          "target": "13-pati-gran"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-estana-del-drac-gall",
+      "name": "Estança del Drac-Gall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -2.170304000437156,
+        "pitch": 0.23771977602334893,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8047479271940627,
+          "pitch": 0.2529913401440158,
+          "rotation": 0,
+          "target": "5-estana-del-drac-gall"
+        },
+        {
+          "yaw": -0.8242896152706791,
+          "pitch": 0.03334685892390965,
+          "rotation": 0,
+          "target": "12-pati-gran"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-estana-del-drac-gall",
+      "name": "Estança del Drac-Gall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -1.3764818893616315,
+        "pitch": 0.12042451962649814,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4000589197746152,
+          "pitch": 0.04706240385946714,
+          "rotation": 0,
+          "target": "12-pati-gran"
+        },
+        {
+          "yaw": -2.175132988769862,
+          "pitch": 0.13606273348659137,
+          "rotation": 0,
+          "target": "4-estana-del-drac-gall"
+        },
+        {
+          "yaw": 0.12611179909941228,
+          "pitch": 0.12380304454526936,
+          "rotation": 0,
+          "target": "6-sala-polivalent"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sala-polivalent",
+      "name": "Sala polivalent",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.8104471875831365,
+        "pitch": 0.30857981392351697,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0765688263509183,
+          "pitch": 0.04802367125823892,
+          "rotation": 0,
+          "target": "14-pati-petit"
+        },
+        {
+          "yaw": -2.9945102045915277,
+          "pitch": 0.07761623353752611,
+          "rotation": 0,
+          "target": "8-passads"
+        },
+        {
+          "yaw": 2.2440628209774136,
+          "pitch": 0.2064369170559246,
+          "rotation": 0,
+          "target": "7-dormitori"
+        },
+        {
+          "yaw": -0.00726345381043636,
+          "pitch": 0.10931125895344529,
+          "rotation": 0,
+          "target": "5-estana-del-drac-gall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-dormitori",
+      "name": "Dormitori",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -2.0021292578928325,
+        "pitch": 0.38193995295583605,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.882192581838016,
+          "pitch": 0.2216464690582498,
+          "rotation": 0,
+          "target": "6-sala-polivalent"
+        },
+        {
+          "yaw": 1.9253238733124869,
+          "pitch": 0.2677112016715899,
+          "rotation": 0,
+          "target": "3-estana-dels-nans"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-passads",
+      "name": "Passadís",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.18195079272973302,
+        "pitch": 0.15440151709689687,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6566944332962361,
+          "pitch": 0.27091128628143224,
+          "rotation": 0,
+          "target": "3-estana-dels-nans"
+        },
+        {
+          "yaw": -0.7549339184344817,
+          "pitch": 0.06156356640847882,
+          "rotation": 0,
+          "target": "6-sala-polivalent"
+        },
+        {
+          "yaw": 2.5004041517429645,
+          "pitch": 0.2400457162250369,
+          "rotation": 0,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pati-gran",
+      "name": "Pati Gran",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -2.8645467064978174,
+        "pitch": 0.23139633916667002,
+        "fov": 1.303246018441174
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.959386369629507,
+          "pitch": 0.09856026221805259,
+          "rotation": 0,
+          "target": "2-estana-dels-tabalers"
+        },
+        {
+          "yaw": -2.663591130663219,
+          "pitch": 0.10124336747588103,
+          "rotation": 0,
+          "target": "10-pati-gran"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-pati-gran",
+      "name": "Pati Gran",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -1.5755563156639703,
+        "pitch": 0.023601796770545747,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.397626406476416,
+          "pitch": 0.1111734229668464,
+          "rotation": 0,
+          "target": "13-pati-gran"
+        },
+        {
+          "yaw": -2.3910102100767645,
+          "pitch": 0.07893083500282927,
+          "rotation": 0,
+          "target": "2-estana-dels-tabalers"
+        },
+        {
+          "yaw": -1.802532641432558,
+          "pitch": 0.05735424285037993,
+          "rotation": 0,
+          "target": "3-estana-dels-nans"
+        },
+        {
+          "yaw": 2.4189251529572093,
+          "pitch": 0.1575446219063572,
+          "rotation": 0,
+          "target": "9-pati-gran"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-pati-gran",
+      "name": "Pati Gran",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 0.7409716006194138,
+        "pitch": 0.004217491762050329,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4685025974807235,
+          "pitch": 0.16638418275904598,
+          "rotation": 0,
+          "target": "13-pati-gran"
+        },
+        {
+          "yaw": -0.20684265508540278,
+          "pitch": 0.13309695502928065,
+          "rotation": 0,
+          "target": "10-pati-gran"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-pati-gran",
+      "name": "Pati Gran",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 1.5470364496958933,
+        "pitch": 0.09278481876515521,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9469021086061051,
+          "pitch": 0.1072903388821107,
+          "rotation": 0,
+          "target": "5-estana-del-drac-gall"
+        },
+        {
+          "yaw": 0.7546068017412733,
+          "pitch": 0.10047079429909367,
+          "rotation": 0,
+          "target": "13-pati-gran"
+        },
+        {
+          "yaw": 0.08753466189055992,
+          "pitch": 0.06077426867664215,
+          "rotation": 0,
+          "target": "11-pati-gran"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-pati-gran",
+      "name": "Pati Gran",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 1.7416101235526948,
+        "pitch": 0.35823871202378044,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5840967536404094,
+          "pitch": 0.10793315587746832,
+          "rotation": 0,
+          "target": "10-pati-gran"
+        },
+        {
+          "yaw": 1.5418501621763525,
+          "pitch": 0.14587011920724358,
+          "rotation": 0,
+          "target": "11-pati-gran"
+        },
+        {
+          "yaw": 0.24304619156725416,
+          "pitch": 0.06598343795230122,
+          "rotation": 0,
+          "target": "12-pati-gran"
+        },
+        {
+          "yaw": -2.5309480708621788,
+          "pitch": 0.13921555779543837,
+          "rotation": 0,
+          "target": "3-estana-dels-nans"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-pati-petit",
+      "name": "Pati petit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 1.4574033522295995,
+        "pitch": 0.28244523234597096,
+        "fov": 1.5056445590528575
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.007953342384221,
+          "pitch": 0.18178666124078013,
+          "rotation": 0,
+          "target": "6-sala-polivalent"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Castellnins",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
